a5e4c25beb3ea5037e4df683d930daca4108292b
duvall3 <mjduvall@hawaii.edu>
2017-02-08 18:01:07 -0800

    bugfix

--- bin/semicheck.sh
+++ bin/semicheck.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # semicheck.sh -- script to print out c++ lines that may be missing terminating semicolons
 
-cat $1 | \
+cat $1 |\
 
-  awk -F "//" '{print $1}' | \ 	# remove comments
-  grep -v "^ *$" | \ 		# remove blank (i.e., empty or whitespace-only) lines
-  grep -v "; *$" 		# remove lines ending in semicolons
+  # remove comments
+  awk -F "//" '{print $1}' | \
+  # remove blank (i.e., empty or whitespace-only) lines
+  grep -v "^ *$" | \
+  # remove lines ending in semicolons
+  grep -v "; *$"
 
 # all pau!   )
 exit 0
