353b839311a8924d1d0b50ba334e099cd21b8c83
duvall3 <mjduvall@hawaii.edu>
2017-11-08 18:46:56 -0800

    adding sum.sh

---
+++ bin/sum.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# sum.sh -- prints the sum of a (newline-separated) list of numbers piped through stdin
+
+##Copyright (C) 2017 Mark J. Duvall
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+echo $(( $( tr '\n' '+' $1 | sed s#"\+$"## ) ))
+
+
