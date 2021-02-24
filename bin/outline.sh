b6751aa5fbe1f843395b3aa3ba8d098ebfa3be3d
duvall3 <duvall3@github.com>
2021-02-24 15:35:03 -0600

    outline.sh --> v~1.0

--- bin/outline.sh
+++ bin/outline.sh
@@ -11,11 +11,39 @@ if [ ! $1 ]; then
   exit 1
 fi
 INFILE=$1
-OUTFILE=$(basename $INFILE .tex)"_outline.tex"
-if [ -f $OUTFILE ]; then
-  echo "$OUTFILE already exists."
+INFILE_NAME=$(basename $INFILE .tex)
+INFILE_DIR=$(dirname $INFILE)
+OUTFILE_DIR="outline_"$INFILE_NAME
+OUTFILE=$OUTFILE_DIR".tex"
+if [ -d $OUTFILE_DIR ]; then
+  echo "$OUTFILE_DIR already exists."
   exit 2
+else
+  mkdir $OUTFILE_DIR
 fi
+
+# Makefile
+echo -e "\
+# usage: make clean
+#	 make
+PROJECT=$(basename $OUTFILE .tex)
+SOURCES=\$(*.tex)
+TEX=pdflatex
+#BIBTEX=bibtex
+BUILDTEX=\$(TEX) \$(PROJECT).tex
+
+all: \$(PROJECT).pdf
+
+\$(PROJECT).pdf: \$(SOURCES)
+#all:
+	\$(BUILDTEX)
+#	\$(BIBTEX) \$(PROJECT)
+	\$(BUILDTEX)
+	\$(BUILDTEX)
+
+clean:
+	rm -f *.ps *.dvi *.aux *.log p.ps *backup *~ *.bbl *blg *.pdf *.toc
+" >> $OUTFILE_DIR/Makefile
 
 # LaTeX header
 echo -e "\
@@ -23,23 +51,25 @@ echo -e "\
 \usepackage[utf8]{inputenc}
 \usepackage[T1]{fontenc}
 \usepackage{authblk}\n
+\usepackage{graphicx,hyperref} \hypersetup{colorlinks=true, linkcolor=blue, urlcolor=blue, citecolor=blue}
 \\\begin{document}
-" > $OUTFILE
+" > $OUTFILE_DIR/$OUTFILE
 
 # title, author, etc.
-awk '$0 ~ /^\\(title|author|affil|date|maketitle)/ {print $0}' $INFILE >> $OUTFILE
+awk '$0 ~ /^\\(title|author|affil|date|maketitle)/ {print $0}' $INFILE >> $OUTFILE_DIR/$OUTFILE
 
 # ACTUAL OUTLINE = TOC
-echo -e "\n\\\tableofcontents\n" >> $OUTFILE
+echo -e "\n\\\tableofcontents\n" >> $OUTFILE_DIR/$OUTFILE
 
 # process chapters
 CHAPTERS=$(grep -e "^\\\input{" $INFILE | sed s/'.input.'// | tr -d })
 for FILE in ${CHAPTERS[*]}; do
-  awk '$0 ~ /^\\(chapter|.*section)/ {print $0}' ../$FILE >> $OUTFILE
+  awk '$0 ~ /^\\(chapter|.*section)/ {print $0}' $INFILE_DIR/$FILE >> $OUTFILE_DIR/$OUTFILE
+# echo $FILE >> $OUTFILE_DIR/$OUTFILE #debug
 done
 
 # finish up
-echo -e "\n\\\end{document}" >> $OUTFILE
+echo -e "\n\\\end{document}" >> $OUTFILE_DIR/$OUTFILE
 
 # all pau!   )
 exit 0
