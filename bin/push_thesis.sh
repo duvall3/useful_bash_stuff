2f67001386c4393add02a67711a62dccb3f1c1ff
duvall3 <duvall3@github.com>
2021-05-04 22:39:58 -0500

    adding some important new aliases to newhome/bash_aliases; tweaks to bin/push_thesis.sh and bin/thmake

--- bin/push_thesis.sh
+++ bin/push_thesis.sh
@@ -2,6 +2,21 @@
 # push_thesis.sh -- script for crontab to rsync THESIS
 #   and THESIS_repo to backup locations
 
+##Copyright (C) 2021 Mark J. Duvall
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    (at your option) any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 ## init
 
