3640b4b166c8809a3da877b56731ba05df6a171e
duvall3 <duvall3@github.com>
2021-11-20 20:59:50 -0600

    wrote the actual content for roll.py

--- bin/roll_dir/roll.py
+++ bin/roll_dir/roll.py
@@ -1,17 +1,50 @@
-def roll(n,k=1,b=0):
-    """Return the result of k simulated rolls of an n-sided die,
-    with overall bonus b.
+#!/usr/bin/python3
+"Simulate and combine die rolls and bonuses, as in TTRPGs."
+
+import random
+
+def roll(k, n=None, b=None):
+    """Simulate and combine die rolls and bonuses.
+    roll(N):
+        Show the result of a single roll of an N-sided die.
+    roll(K, N):
+        Tabulate the results of K rolls of an N-sided die.
+    roll(K, N, B):
+        Tabulate the results of K rolls of an N-sided die,
+        with an overall bonus B.
+    Summary:
+      |    1dN    =    roll(N)
+      |    KdN    =   roll(K, N)
+      |  KdN + B  =  roll(K, N, B)
     """
 
     # arg check
-    pass
+    if n==None:
+        if b==None: # plain single roll
+            return round( random.uniform(1, k) )
+    else:
+        if b==None: # no bonus entered
+            b = 0
 
     # init
+    request = f'{k}d{n} + {{{b}}}'
+    rolls = []
 
     # generate and store appropriate random integers
+    for j in range(k):
+        rolls.append( round( random.uniform(1,n) ) )
 
     # sum (if not yet)
+    total = sum(rolls) + b
 
     # print and return results
+    print(request + '  ==>  ', end='')
+    for j in range(len(rolls)-1):
+        print(f'{rolls[j]} + ', end='')
+#   print(f'{rolls[-1]} + {{{b}}}  =  {total}')
+    print(f'{rolls[-1]} + {{{b}}}  =  ', end='')
+    return total
 
-    # all pau!   )
+#end
+
+# all pau!   )
