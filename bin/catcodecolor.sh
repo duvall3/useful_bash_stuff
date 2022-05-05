8f8d7e13618afefd3baf2d78dce1bbce4f9e8455
duvall3 <duvall3@github.com>
2022-05-05 11:50:28 -0500

    adding aliases 'lse' and 'lses' to main.txt

---
+++ bin/catcodecolor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# catcodecolor -- moderately-silly script to steadily print a syntax-highlighted
+#   source/code file to stdout
+# USAGE: catcodecolor <FILE> <LEXER>
+# -- where "LEXER" is a language specifier -- e.g., cpp, html, [ba]sh, py[thon], etc.
+# -- ex.: catcodecolor example.sh sh
+
+FILE_LINES=$(wc -l $1 | awk '{print $1}')
+for (( LINE_NO = 1; LINE_NO < $FILE_LINES; LINE_NO++ )) {
+  awk -v lineNo=$LINE_NO 'NR == lineNo {print}' $1 | pygmentize -s -O style=monokai -l $2
+sleep .10s
+}
+
+# all pau!   )
+exit 0
