592479b006c6f5b3cbdc5f5f65aa18f213d0c3ed
duvall3 <duvall3@github.com>
2022-06-21 20:41:36 -0500

    various minor changes to utility scripts

--- bin/catcodecolor.sh
+++ bin/catcodecolor.sh
@@ -5,6 +5,11 @@
 # -- where "LEXER" is a language specifier -- e.g., cpp, html, [ba]sh, py[thon], etc.
 # -- ex.: catcodecolor example.sh sh
 
+if [ $# -eq 0 ]; then
+  echo "Usage: catcodecolor <FILE> <LEXER>"
+  exit 2
+fi
+
 FILE_LINES=$(wc -l $1 | awk '{print $1}')
 for (( LINE_NO = 1; LINE_NO < $FILE_LINES; LINE_NO++ )) {
   awk -v lineNo=$LINE_NO 'NR == lineNo {print}' $1 | pygmentize -s -O style=monokai -l $2
