ef36870ecae59fa3493ed162529cbff786eaf36a
duvall3 <duvall3@github.com>
2022-08-26 13:29:15 -0500

    improvements to cpu_mon.sh and pcpu

--- bin/cpu_mon.sh
+++ bin/cpu_mon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# heat_mon.sh -- very simple script to track CPU temperature and usage
+# cpu_mon.sh -- very simple script to track CPU temperature and usage
 #     via acpi and iostat (sysstat), respectively
 # -- runs until terminated via <C-c>
 #
-# usage: heat_mon.sh [INTERVAL]
+# usage: cpu_mon.sh [INTERVAL]
 # - INTERVAL is assumed to be in seconds; defaults to 15 if not specified
 # - format for INTERVAL is same as "date" command -- e.g., "20m" = "twenty minutes", etc.
 # - the 'acpi' package is required, but if you don't already have it, you really should get it anyway
@@ -25,9 +25,15 @@
 #     You should have received a copy of the GNU General Public License
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# # program check
+# if [[ ! $(which iostat) || ! $(which acpi) ]]; then
+#   echo "ERROR: This script requires both 'iostat' and 'acpi'; at least one of these was not found." && return 2;
+# fi
+
+
 # program check
-if [[ ! $(which iostat) || ! $(which acpi) ]]; then
-  echo "ERROR: This script requires both 'iostat' and 'acpi'; at least one of these was not found." && return 2;
+if [[ ! $(which acpi) ]]; then
+  echo "ERROR: This script requires 'acpi'." && return 2;
 fi
 
 # arg check
@@ -41,7 +47,9 @@ fi
 echo
 while true; do
 	time=$(date +%H:%M:%S)
-	util=$(iostat -cy 1 1 | awk '$2 ~ /user/ {getline; print $1}')
+	# util=$(iostat -cy 1 1 | awk '$2 ~ /user/ {getline; print $1}')
+	util=$(ps -eo | grep -Ev "[[:alpha:]]" | add_list.sh)
+	util=$(echo "$util / 64.0" | bc -l)
 	temp=$(acpi -t | grep -Eo "[[:digit:]]+\.[[:digit:]]")
 	printf "%2.f%c  %2.fC  %s\n" $util '%' $temp $time
 	sleep $sleep
