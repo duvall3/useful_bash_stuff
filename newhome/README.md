e6a1db202b7983623f75aba6879c588c98bc09b3
duvall3 <duvall3@github.com>
2022-10-30 12:46:07 -0500

    adding install_newhome.sh to newhome/ and updating its README

--- newhome/README.md
+++ newhome/README.md
@@ -3,3 +3,15 @@
 These are the files that go into my home directory the minute I get a new account on any system.
 They include settings for aliases, command history, syntax highlighting, and more.
 Personalize them and use them whenever you get a new login!
+
+Install them by running `./install_newhome' in this directory.
+To force overwriting of destination files (i.e., to answer 'yes' to all the "overwrite?" prompts), run as `./install_newhome -f`.
+
+*Tip:* If you've already copied the files to your `$HOME` directory, you can install them with the following command:
+```bash
+rename 's/(.*)/.$1/' {bash,input,screen,vim}rc bash_aliases vim/
+```
+If needed, you can undo this with the following command:
+```bash
+rename 's/\.//' .{bash,input,screen,vim}rc .bash_aliases .vim/
+```
---
+++ newhome/install_newhome.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# install_newhome.sh -- simple script to install the user-config files
+#   (.*rc, etc) from this directory into the $USER's $HOME directory
+# USAGE: ./install_newhome.sh [-f]
+# -- Run with option '-f' to force overwriting of destination files
+# ~ Mark J. Duvall ~ mjduvall@hawaii.edu ~ 10/2022 ~ #
+
+#Copyright (C) 2022 Mark J. Duvall / T. Rocks Science
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# dir check
+if [[ ! $(pwd) =~ useful_bash_stuff/newhome ]]; then
+  echo "Please run this script from the 'useful_bash_stuff/newhome' directory."
+  echo "USAGE: ./install_newhome.sh [-f]"
+  exit 5
+fi
+
+# arg check
+if [[ $1 =~ -f ]]; then
+  OPTION="-f"
+else
+  echo "USAGE: ./install_newhome.sh [-f]"
+  exit 10
+fi
+
+# main
+for FILE in {bashrc,bash_aliases,inputrc,screenrc,vim/,vimrc}; do
+  cp -ir $OPTION $FILE $HOME/.$FILE
+done
+
+# all pau!   )
+exit 0
